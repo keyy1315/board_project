40ea32563d7a5acdb07d2baa92e1d887ef7f9a11
keyy1315 <lovelina1315@gmail.com>
2025-02-13 18:14:39 +0900

    chore : initial spring boot setting

---
+++ src/main/java/org/example/board_project/BoardProjectApplication.java
@@ -0,0 +1,15 @@
+package org.example.board_project;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan
+public class BoardProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardProjectApplication.class, args);
+    }
+
+}
