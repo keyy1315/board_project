3f3672702f3386b655a32410edb049d374197e5b
keyy1315 <lovelina1315@gmail.com>
2025-02-17 13:00:51 +0900

    fix : fixed throw Error type
    빈 값이 입력되었을 때 프론트에 전달 안되던 IllegalArgumentException 을 custom Exception 으로 변경하여 global handling 에서 intercept 하여 처리하도록 수정

--- src/main/java/org/example/board_project/service/BoardService.java
+++ src/main/java/org/example/board_project/service/BoardService.java
@@ -10,6 +10,7 @@ import org.example.board_project.model.dto.responseDTO.BoardListResponseDTO;
 import org.example.board_project.model.dto.responseDTO.BoardResponseDTO;
 import org.example.board_project.model.entity.Board;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
@@ -22,13 +23,13 @@ public class BoardService {
     private final BoardMapper boardMapper;
 
     /**
-     * @param dto                   - [category_cd, searchCode, search, sortCode, offset, size]
-     *                              - category_cd  : 카테고리 코드
-     *                              - searchCode   : 검색 조건
-     *                              - search       : 검색어
-     *                              - sortCode     : 정렬조건
-     *                              - offset       : 데이터 출력 시작 인덱스 (ex: offset=3, 4번부터 출력)
-     *                              - size         : 게시글 출력 개수
+     * @param dto - [category_cd, searchCode, search, sortCode, offset, size]
+     *            - category_cd  : 카테고리 코드
+     *            - searchCode   : 검색 조건
+     *            - search       : 검색어
+     *            - sortCode     : 정렬조건
+     *            - offset       : 데이터 출력 시작 인덱스 (ex: offset=3, 4번부터 출력)
+     *            - size         : 게시글 출력 개수
      * @return BoardListResponseDTO - [BoardResponseDTO 의 List, 게시글 총 개수]
      * BoardResponseDTO (비밀번호 컬럼이 없는 DTO) 에 게시글의 정보를 담아
      * BoardListResponseDTO 에 찾은 게시글의 총 개수와 함께 List type 으로 담아 반환함
@@ -55,9 +56,10 @@ public class BoardService {
     }
 
     /**
-     * @param dto               - [작성자명, 비밀번호, 제목, 내용, 카테고리 코드, 게시글 번호]
+     * @param dto - [작성자명, 비밀번호, 제목, 내용, 카테고리 코드, 게시글 번호]
      * @return BoardResponseDTO - [게시글 번호, 카데고리 코드, 제목, 내용, 작성자명, 조회수, 작성 날짜, 수정 날짜]
      */
+    @Transactional
     public BoardResponseDTO saveBoard(WriteBoardRequestDTO dto) {
         DTONullChecker(dto);
         if (boardMapper.saveBoard(dto) == 1) {
@@ -72,9 +74,10 @@ public class BoardService {
     }
 
     /**
-     * @param no                - 게시글 번호
+     * @param no - 게시글 번호
      * @return BoardResponseDTO - [게시글 번호, 카데고리 코드, 제목, 내용, 작성자명, 조회수, 작성 날짜, 수정 날짜]
      */
+    @Transactional
     public BoardResponseDTO findBoard(int no) {
         if (boardMapper.IncrementViewCnt(no) == 1) {
             Board board = boardMapper.findBoard(no);
@@ -88,9 +91,10 @@ public class BoardService {
     }
 
     /**
-     * @param dto               - [작성자명, 비밀번호, 제목, 내용, 카테고리 코드, 게시글 번호]
+     * @param dto - [작성자명, 비밀번호, 제목, 내용, 카테고리 코드, 게시글 번호]
      * @return BoardResponseDTO - [게시글 번호, 카데고리 코드, 제목, 내용, 작성자명, 조회수, 작성 날짜, 수정 날짜]
      */
+    @Transactional
     public BoardResponseDTO updateBoard(WriteBoardRequestDTO dto) {
         if (boardMapper.findBoard(dto.getBoardNo()) == null) {
             throw new BoardException(
@@ -115,6 +119,7 @@ public class BoardService {
      * @return boolean
      * 삭제된 행의 개수가 1개면 true 반환
      */
+    @Transactional
     public Boolean deleteBoard(int no) {
         if (boardMapper.deleteBoard(no) == 1) {
             return true;
@@ -127,7 +132,31 @@ public class BoardService {
     }
 
     /**
-     * @param board             - Entity
+     * @param no       - 게시글 번호
+     * @param password - 게시글 비밀번호
+     * @return boolean - 비밀번호 일치 여부
+     * 비밀번호에 빈 값이 들어오면 BoardException (400) 반환
+     * 게시글 번호에 비밀번호가 저장되지 않았거나 찾을 수 없는 게시글이라면 NonExistent Board Exception (404) 반환
+     * DB 에 저장된 비밀번호와 일치하다면 true 반환
+     */
+    public boolean authBoard(int no, String password) {
+        if (!StringUtils.hasText(password)) throw new BoardException(
+                ErrorCode.DTO_OBJECT_IS_EMPTY.getHttpStatus(),
+                "password is empty"
+        );
+        String db_pwd = boardMapper.getBoardPassword(no);
+        if (db_pwd == null) {
+            throw new BoardException(
+                    ErrorCode.NONEXISTENT_BOARD.getHttpStatus(),
+                    "Cannot find Board or Board Password"
+            );
+        }
+
+        return db_pwd.equals(password);
+    }
+
+    /**
+     * @param board - Entity
      * @return BoardResponseDTO - [게시글 번호, 카데고리 코드, 제목, 내용, 작성자명, 조회수, 작성 날짜, 수정 날짜]
      * DB에 담긴 비밀번호를 프론트에 넘기지 않기 위해 Board Entity 를 DTO 로 변환하는 메소드
      */
@@ -146,28 +175,30 @@ public class BoardService {
 
     /**
      * @param dto - WriteBoardRequestDTO
-     * 클라이언트에게 받은 데이터에 빈 값이 있는지 확인하는 메소드
-     * 빈 값이 있다면 NullPointerException 반환한다.
+     *            클라이언트에게 받은 데이터에 빈 값이 있는지 확인하는 메소드
+     *            빈 값이 있다면 BoardException (Bad Request error (400))을 반환한다.
      */
     private void DTONullChecker(WriteBoardRequestDTO dto) {
-        if (!StringUtils.hasText(dto.getWriter_nm())) throw new IllegalArgumentException("writer name is empty");
-        if (!StringUtils.hasText(dto.getPassword())) throw new IllegalArgumentException("password is empty");
-        if (!StringUtils.hasText(dto.getCategory_cd())) throw new IllegalArgumentException("category code is empty");
-        if (!StringUtils.hasText(dto.getCont())) throw new IllegalArgumentException("content is empty");
-        if (!StringUtils.hasText(dto.getTitle())) throw new IllegalArgumentException("title is empty");
+        if (!StringUtils.hasText(dto.getWriter_nm())) throw new BoardException(
+                ErrorCode.DTO_OBJECT_IS_EMPTY.getHttpStatus(),
+                "Writer name is empty"
+        );
+        if (!StringUtils.hasText(dto.getPassword())) throw new BoardException(
+                ErrorCode.DTO_OBJECT_IS_EMPTY.getHttpStatus(),
+                "Password is empty"
+        );
+        if (!StringUtils.hasText(dto.getCategory_cd())) throw new BoardException(
+                ErrorCode.DTO_OBJECT_IS_EMPTY.getHttpStatus(),
+                "Category Code is empty"
+        );
+        if (!StringUtils.hasText(dto.getCont())) throw new BoardException(
+                ErrorCode.DTO_OBJECT_IS_EMPTY.getHttpStatus(),
+                "Content is empty"
+        );
+        if (!StringUtils.hasText(dto.getTitle())) throw new BoardException(
+                ErrorCode.DTO_OBJECT_IS_EMPTY.getHttpStatus(),
+                "Title is empty"
+        );
     }
 
-    /**
-     * @param no       - 게시글 번호
-     * @param password - 게시글 비밀번호
-     * @return boolean - 비밀번호 일치 여부
-     * 비밀번호에 빈 값이 들어오면 NullPointerException 반환
-     * DB 에 저장된 비밀번호와 일치하다면 true 반환
-     */
-    public boolean authBoard(int no, String password) {
-        if (!StringUtils.hasText(password)) throw new IllegalArgumentException("password is empty");
-        String db_pwd = boardMapper.getBoardPassword(no);
-
-        return db_pwd.equals(password);
-    }
 }
