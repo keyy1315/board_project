6c3a6abf278eb1b4e0d4e1330a2b4b5a0b108612
keyy1315 <lovelina1315@gmail.com>
2025-02-18 18:06:09 +0900

    chore : initialize react proxy server

--- src/main/frontend/src/App.tsx
+++ src/main/frontend/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./component/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./component/Footer";
+import "./css/style.css";
+import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import SideBar from "./component/SideBar";
+import BoardList from "./pages/BoardList";
+import Board from "./pages/Board";
+import BoardWrite from "./pages/BoardWrite";
 
+const queryClient = new QueryClient();
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<SideBar />}>
+              <Route index element={<BoardList />} />
+              <Route path="board/:no" element={<Board />} />
+              <Route path="board/write" element={<BoardWrite />}/>
+            </Route>
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </RecoilRoot>
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
