c09a150fe290b1fbb9ab97da764b52e5e14a6a5f
keyy1315 <lovelina1315@gmail.com>
2025-02-19 17:17:00 +0900

    feat : implement File DTO and static factory method at WriteBoardRequestDTO

--- src/main/java/org/example/board_project/model/dto/requestDTO/board/WriteBoardRequestDTO.java
+++ src/main/java/org/example/board_project/model/dto/requestDTO/board/WriteBoardRequestDTO.java
@@ -1,13 +1,17 @@
 package org.example.board_project.model.dto.requestDTO.board;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import org.example.board_project.model.dto.requestDTO.file.FileRequestDTO;
+import org.example.board_project.model.entity.Board;
 
 /**
  * 글 작성/수정 요청할 때 전송하는 데이터 객체
  */
 @AllArgsConstructor
 @Getter
+@Builder
 public class WriteBoardRequestDTO {
     private String writer_nm;
     private String password;
@@ -15,4 +19,17 @@ public class WriteBoardRequestDTO {
     private String cont;
     private String category_cd;
     private int boardNo;
+    private FileRequestDTO file;
+
+    public static WriteBoardRequestDTO from(int boardNo, WriteBoardRequestDTO dto) {
+        return WriteBoardRequestDTO.builder()
+                .boardNo(boardNo)
+                .writer_nm(dto.getWriter_nm())
+                .password(dto.getPassword())
+                .title(dto.getTitle())
+                .cont(dto.getCont())
+                .category_cd(dto.getCategory_cd())
+                .file(dto.getFile())
+                .build();
+    }
 }
