b8696238912e9cbc9a41d69b66b3bcdbf42f02b9
keyy1315 <lovelina1315@gmail.com>
2025-02-19 13:54:08 +0900

    chore : add react-query devtools module

--- src/main/frontend/src/App.tsx
+++ src/main/frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Footer from "./component/Footer";
 import "./css/style.css";
 import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import SideBar from "./component/SideBar";
 import BoardList from "./pages/BoardList";
 import Board from "./pages/Board";
@@ -13,6 +14,7 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <RecoilRoot>
         <BrowserRouter>
           <Header />
